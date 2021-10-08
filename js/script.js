56fe1876465e5d591a1d6fedeb098c73d0a30d54
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-08 15:04:57 +0200

    Fixed error in bonus task

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 let msgAlert = "L'età e/o il numero di km inseriti non sono validi.";
-let eta = prompt("Buongiorno! Quanti anni");
+let eta = prompt("Buongiorno! Quanti anni hai?");
 let km = prompt("Quanti kilometri desideri percorrere?");
 
-/* inizio controlli */
+
 eta = parseInt(eta);
 km = parseInt(km);
-console.log("Età:" + eta, "km:" + km);
-
-if (isNaN(eta) || isNaN(km)){
-    alert(msgAlert);
-}
-/* fine controlli */
-
-
 
 const costKm = 0.21;
 let costoTot = km * costKm;
-console.log("Costo totale:" + costoTot);
 
-if (eta < 18) {
-    costoTot = costoTot * .8
+
+let msgAlertPrice = `Il prezzo del biglietto per il costo del tuo viaggio di ${km} km è pari a: ${costoTot.toFixed(2)}€`;
+
+if (isNaN(eta) || isNaN(km)){
+    alert(msgAlert);
+} else if (eta < 18) {
+    costoTot = costoTot * .8;
+    alert(msgAlertPrice);
 } else if (eta >= 65) {
     costoTot = costoTot * .6
+    alert(msgAlertPrice);
+} else {
+    alert(msgAlertPrice);
 }
-console.log("Costo totale scontato:" + costoTot.toFixed(2))
-
-alert( `Il prezzo del biglietto per il costo del tuo viaggio di ${km} km è pari a: ${costoTot.toFixed(2)}€`); 
